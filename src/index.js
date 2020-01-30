7ec80a1a86a38651787511f8fb5b4009726852cd
Pat Brown <ptrckbrown94@gmail.com>
2020-01-30 12:14:02 -0600

    updated index

--- src/index.js
+++ src/index.js
@@ -2,6 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 import App from "./App";
+import Axios from "axios";
+
+//Health Check
+Axios.get("localhost:5000/ping").then(() => {
+console.log("should be working")
+}).catch(() => {
+    console.log("not working")
+});
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(<App />, rootElement);
